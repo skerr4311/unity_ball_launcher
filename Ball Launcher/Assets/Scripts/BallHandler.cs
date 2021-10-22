8c77deba4c2a77c9134f4d9f88790444aa03131f
Steve <sker078@aucklanduni.ac.nz>
2021-10-23 10:19:09 +1300

    Move Objects With Touch

--- Ball Launcher/Assets/Scripts/BallHandler.cs
+++ Ball Launcher/Assets/Scripts/BallHandler.cs
@@ -5,6 +5,7 @@ using UnityEngine.InputSystem;
 
 public class BallHandler : MonoBehaviour
 {
+    [SerializeField] private Rigidbody2D currentBallRigidbody;
     private Camera mainCamera;
     // Start is called before the first frame update
     void Start()
@@ -16,11 +17,12 @@ public class BallHandler : MonoBehaviour
     void Update()
     {
         if(!Touchscreen.current.primaryTouch.press.isPressed){
+            currentBallRigidbody.isKinematic = false;
             return;
         }
-
+        currentBallRigidbody.isKinematic = true;
         Vector2 touchPosition = Touchscreen.current.primaryTouch.position.ReadValue();
         Vector3 worldPosition = mainCamera.ScreenToWorldPoint(touchPosition);
-        Debug.Log(worldPosition);
+        currentBallRigidbody.position = worldPosition;
     }
 }
